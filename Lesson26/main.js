a0e68238b586d3228d6c469a90bb62daf80f0413
AbeekOfficial <abeekoffi@gmail.com>
2023-10-09 16:37:36 +0500

    js files

---
+++ Lesson26/main.js
@@ -0,0 +1,191 @@
+// If 1.
+/*let n = parseInt(prompt("Sonni yozing"));
+if (n > 0) {
+     alert (++n);
+}
+else (n < 0) 
+        alert (n); */ 
+
+
+// If 2.
+/*let n = parseInt(prompt("Sonni yozing"));
+if (n > 0) {
+     alert (++n);
+}
+else (n < 0) 
+        alert (n - 2); */     
+
+// If 3.
+/*let n = parseInt(prompt("Sonni yozing"));
+if (n > 0) {
+     alert (++n);
+}
+else if (n < 0) {
+     alert (n - 2); 
+}
+else (n === 0) 
+     alert (n + 10);*/
+
+// If 4.
+/*let a = parseInt(prompt("1-Sonni yozing"));
+let b = parseInt(prompt("2-Sonni yozing"));
+let c = parseInt(prompt("3-Sonni yozing"));
+let x = 0;
+if (a > 0) {
+     alert(++x);
+}
+if (b > 0) {
+     alert(++x);
+}
+if (c > 0) {
+     alert(++x);
+}*/
+
+// If 5.
+/*let a = parseInt(prompt("1-Sonni yozing"));
+let b = parseInt(prompt("2-Sonni yozing"));
+let c = parseInt(prompt("3-Sonni yozing"));
+let x = 0;
+let y = 0;
+if (a > 0) {
+     alert(++x);
+}
+if (b > 0) {
+     alert(++x);
+}
+if (c > 0) {
+     alert(++x);
+}
+
+if (a < 0) {
+     alert(++y);
+}
+if (b < 0) {
+     alert(++y);
+}
+if (c < 0) {
+     alert(++y);
+}*/
+
+// If 6.
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+
+if (a > b) {
+     alert("a son b sondan katta");
+}
+else if (a == b) {
+     alert ("a son b songa teng");
+}
+else  {
+     alert("a son b sondan kichik");
+}*/
+
+
+// If 7. 
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+let x = 1;
+if (a > b) {
+     alert(++x);
+}
+else if (a == b) {
+     alert("ular o'zaro teng");
+}
+else {
+     alert(x);
+}*/
+
+// If 8. 
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+if (a > b) {
+     alert(a);
+     alert(b);
+}
+else {
+     alert(b);
+     alert(a);
+}*/
+
+// If 9. 
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+let c;
+if (a>b) {
+     c == a;
+     b == a;
+     b == c;
+     alert(a);
+     alert(b);
+}
+else {
+     alert(a);
+     alert(b);
+}*/
+
+// If 10. 
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+if (a !== b) {
+     alert(a +b);
+}
+else {
+     alert(a+0);
+     alert(b+0);
+}*/
+
+// Booleaen 1.
+/*let a = parseInt(prompt("a sonni yozing"));
+alert(a>0);*/
+
+// Booleaen 2.
+/*let a = parseInt(prompt("a sonni yozing"));
+if (a % 2 == 1){
+     alert(true)
+}
+else {
+     alert(false)
+}*/
+
+// Booleaen 3.
+/*let a = parseInt(prompt("a sonni yozing"));
+if (a % 2 == 0){
+     alert(true)
+}
+else {
+     alert(false)
+}*/
+
+
+// Booleaen 4.
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+if (a > 2 && b <= 3){
+     alert(true);
+}
+else {
+     alert(false);
+}*/
+
+// Booleaen 5.
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+if (a >= || b < -2){
+     alert(true);
+}
+else {
+     alert(false);
+}*/
+
+
+// Booleaen 6.
+/*let a = parseInt(prompt("a sonni yozing"));
+let b = parseInt(prompt("b sonni yozing"));
+let c = parseInt(prompt("c sonni yozing"));
+if (a<=b<=c){
+     alert(true);
+}
+else {
+     alert(false);
+}*/
