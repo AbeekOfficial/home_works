e58f002be9553accdaed6c561bbd48bf6e60f58a
AbeekOfficial <abeekoffi@gmail.com>
2023-10-13 18:57:08 +0500

    while

---
+++ lesson28/main.js
@@ -0,0 +1,72 @@
+// While 1.
+// let k = 6;
+// let n = 2;
+// let i = 1;
+// while (i <= n) {
+//      console.log(k);
+//      i++;
+// }
+
+
+// While 2.
+// let a = 1;
+// let b = 4;
+// let i = a;
+// while (i <= b) {
+//      console.log(i);
+//      i++;
+// }
+
+
+// While 3.
+// let a = 3, b = 12;
+
+// if (a > b) {
+//      let i = a - 1;
+//      while (i > b) {
+//          console.log(i);
+//          i--;
+//      }
+//  } else if (b > a) {
+//      let i = b - 1;
+//      while (i > a) {
+//          console.log(i);
+//          i--;
+//      }
+//  } else {
+//      console.log("a va b teng sonlar");
+//  }
+
+
+// While 4.
+// const narx = 1;
+
+// let kg = 1;
+// while (kg <= 10) {
+//      const UmumiyNarx = kg*narx;
+//      console.log(UmumiyNarx);
+//      kg++;
+// }
+
+
+// While 5.
+// const narx = 10;
+
+// let kg = parseFloat(0.1);
+// while (kg <= 1) {
+//      const UmumiyNarx = kg*narx;
+//      console.log(UmumiyNarx + ' so`m');     
+//      kg++;
+// }
+
+
+// while 7.
+let a = 1;
+let b = 3;
+let i = a;
+while (i <= b) {
+     console.log(i)
+      i++;
+}
+const UmumiySon = a + i;
+console.log(UmumiySon);
