1a12320135302129b3aead1e1f9c5734d04da64f
AbeekOfficial <abeekoffi@gmail.com>
2023-10-16 17:11:14 +0500

    functions

---
+++ lesson29/main.js
@@ -0,0 +1,21 @@
+1.
+function isEven(num) {
+     return num % 2 === 0;
+   }
+   
+   console.log(isEven(24)); 
+   console.log(isEven(11));
+   console.log(isEven(17));
+   console.log(isEven(21));
+   console.log(isEven(8));
+
+//    isEven();
+
+2.
+const hisoblash = function() {
+     return 5;
+}
+
+console.log(hisoblash());
+
+hisoblash();
